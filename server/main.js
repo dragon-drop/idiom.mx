e980ae18fc90c88263e20eaac518aae4f5851006
Adam Dawkins <adam@dragondrop.uk>
2017-12-19 19:33:34 +0000

    Generate the idiots client-side (make things a lot faster!)

--- server/main.js
+++ server/main.js
@@ -1,10 +1,6 @@
 import publications from './publications';
-import methods from './methods';
 import seeds from './seeds';
-import nlp from 'nlp_compromise';
 
-methods();
 seeds();
 publications();
-console.log(nlp.noun('pope').article());
 
